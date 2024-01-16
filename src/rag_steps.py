2eaf7c4380c930d80b8269881dd50ec851a15850
Mukund Iyer <msiyer@student.ubc.ca>
2024-01-15 16:05:07 -0800

    feat: funcitons for RAG

--- src/rag_steps.py
+++ src/rag_steps.py
@@ -2,6 +2,7 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from langchain.chains.combine_documents.stuff import StuffDocumentsChain
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.vectorstores import Chroma
 
 def create_conversational_chain() -> StuffDocumentsChain:
     """
@@ -49,10 +50,34 @@ def create_conversational_chain() -> StuffDocumentsChain:
 
     return chain
 
-def get_response(user_question, vector_store):
+def get_response(user_question: str, vector_store: Chroma) -> str:
+    """
+    Retrieves and generates a response to a user's question using a conversational chain.
+
+    Parameters:
+        user_question (str): The user's question for which a response is sought.
+
+        vector_store (Chroma): A vector store containing pre-processed text chunks.
+
+    Returns:
+        str: The generated response to the user's question.
+
+    Notes:
+        - The function uses the vector_store to retrieve relevant documents for the user's
+          question using a specified retriever with search type 'mmr' and search kwargs.
+        - A conversational chain is then created using the create_conversational_chain
+          function, and the user's question along with the retrieved context is passed to
+          the chain for generating a response.
+        - Adjust the search_type and search_kwargs in the retriever to customize the document
+          retrieval process.
+        - Ensure that the vector_store, chain, and retriever are properly configured and
+          imported from the relevant modules.
+    """
+
     retriever = vector_store.as_retriever(search_type='mmr', search_kwargs={"k":10})
     context = retriever.get_relevant_documents(user_question)
     chain = create_conversational_chain()
     stuff_answer = chain({"context": context, "question": user_question},
                          return_only_outputs=True)
+    
     return stuff_answer
